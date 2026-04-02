6a3c4e02501f8c47b9e33a0436e2d417cc634f07
Konrad <zwierzchowskikon@gmail.com>
2026-04-02 22:44:21 +0200

    Add project files.

---
+++ ConsoleApp1/BinaryGap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BinaryGap
+{
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+        }
+
+        public int solution(int N)
+        {
+            string binaryString = Convert.ToString(N, 2);
+            char[] binary = binaryString.ToArray();
+
+            int currentCount = 0;
+            int maxCount = 0;
+
+            foreach (var bin in binary)
+            {
+                if (bin == '0')
+                    currentCount++;
+                else
+                {
+                    if (currentCount > maxCount)
+                        maxCount = currentCount;
+                    currentCount = 0;
+                }
+            }
+
+            return maxCount;
+        }
+    }
+}
---
+++ ProgrammingPuzzles.xUnit/BinaryGapFactTests.cs
@@ -0,0 +1,41 @@
+﻿using BinaryGap;
+
+namespace ProgrammingPuzzles.xUnit
+{
+    public class BinaryGapFactTests
+    {
+        [Fact]
+        public void solution_ValueOf11_Returns1()
+        {
+            // Arrange
+            var solution = new Solution();
+            // Act
+            var result = solution.solution(11);
+            // Assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void solution_ValueOf1041_Returns5()
+        {
+            // Arrange
+            var solution = new Solution();
+            // Act
+            var result = solution.solution(1041);
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void solution_ValueOf15_Returns0()
+        {
+            // Arrange
+            var solution = new Solution();
+            // Act
+            var result = solution.solution(15);
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+    }
+}
