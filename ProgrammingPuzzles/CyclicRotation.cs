95d8712212406c414e707990aeaf1611acbc7ecc
Konrad <zwierzchowskikon@gmail.com>
2026-04-03 13:16:10 +0200

    feat(CyclicRotation): File added

---
+++ ProgrammingPuzzles/CyclicRotation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyclicRotations
+{
+    class Solution
+    {
+        public int[] solution(int[] A, int K)
+        {
+            int[] result = new int[A.Length];
+            for (int i = 0; i < K; i++)
+            {
+                int movedBy = i + K;
+                if (movedBy >= A.Length)
+                    movedBy -= A.Length;
+
+                result[movedBy] = A[i];
+            }
+
+            return result;
+        }
+
+    }
+}
+
