67c9c1fd208b8cca3c4fce08607b9da60c8c0979
Konrad <zwierzchowskikon@gmail.com>
2026-04-03 14:02:21 +0200

    feat(CyclicRotations): code cleanup; add fact tests

---
+++ ProgrammingPuzzles.xUnit/CyclicRotationFactTests.cs
@@ -0,0 +1,59 @@
+﻿using CyclicRotationsNamespace;
+
+namespace ProgrammingPuzzles.xUnit
+{
+    public class CyclicRotationFactTests
+    {
+        [Fact]
+        public void solution_ValueOf3_8_9_7_6_Mod3_Returns9_7_6_3_8()
+        {
+            // Arrange
+            var cyclicRotations = new CyclicRotations();
+            var input = new int[] { 3, 8, 9, 7, 6 };
+            int mod = 3;
+            // Act
+            var result = cyclicRotations.solution(input, mod);
+            // Assert
+            Assert.Equal(new int[] { 9, 7, 6, 3, 8 }, result);
+        }
+
+        [Fact]
+        public void solution_ValueOf1_2_3_4_Mod4_Returns1_2_3_4()
+        {
+            // Arrange
+            var cyclicRotations = new CyclicRotations();
+            var input = new int[] { 1, 2, 3, 4 };
+            int mod = 4;
+            // Act
+            var result = cyclicRotations.solution(input, mod);
+            // Assert
+            Assert.Equal(new int[] { 1, 2, 3, 4 }, result);
+        }
+
+        [Fact]
+        public void solution_ValueOf0_0_0_Mod1_Returns0_0_0()
+        {
+            // Arrange
+            var cyclicRotations = new CyclicRotations();
+            var input = new int[] { 0, 0, 0 };
+            int mod = 1;
+            // Act
+            var result = cyclicRotations.solution(input, mod);
+            // Assert
+            Assert.Equal(new int[] { 0, 0, 0 }, result);
+        }
+
+        [Fact]
+        public void solution_ValueOf1_2_3_4_5_Mod4_Returns2_3_4_5_1()
+        {
+            // Arrange
+            var cyclicRotations = new CyclicRotations();
+            var input = new int[] { 1, 2, 3, 4, 5 };
+            int mod = 4;
+            // Act
+            var result = cyclicRotations.solution(input, mod);
+            // Assert
+            Assert.Equal(new int[] { 2, 3, 4, 5, 1 }, result);
+        }
+    }
+}
--- ProgrammingPuzzles/CyclicRotation.cs
+++ ProgrammingPuzzles/CyclicRotation.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CyclicRotations
+namespace CyclicRotationsNamespace
 {
-    class Solution
+    public class CyclicRotations
     {
         public int[] solution(int[] A, int K)
         {
             int[] result = new int[A.Length];
-            for (int i = 0; i < K; i++)
+            for (int i = 0; i < A.Length; i++)
             {
                 int movedBy = i + K;
                 if (movedBy >= A.Length)
